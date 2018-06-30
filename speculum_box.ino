c2c5cc2247f39d961409a2f94a284b0dfeca752f
Tom Chambers <tom.chambers@gmail.com>
2018-06-30 13:33:08 +0100

    First commit - cleaned

---
+++ speculum_box.ino
@@ -0,0 +1,76 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+const int servo1Pin = 6;
+const int servo2Pin = 9;
+const int servo3Pin = 10;
+const int servo4Pin = 11;
+
+int threshold = 750;
+
+int previousLDRValue, currentLDRValue;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  previousLDRValue = 0;
+  currentLDRValue = 0;
+  
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
+  servo3.attach(servo3Pin);
+  servo4.attach(servo4Pin);
+
+  //Start Positions
+  servo1.write(80);
+  servo2.write(73);
+  servo3.write(95);
+  servo4.write(128);
+
+  previousLDRValue = analogRead(A0);
+  currentLDRValue = analogRead(A0);
+}
+
+void loop() {
+
+  currentLDRValue = analogRead(A0);
+
+  int delta = previousLDRValue - currentLDRValue;
+  if (delta > 100) {
+    doRun();
+  }
+  
+  
+  previousLDRValue = currentLDRValue;
+}
+
+void doRun(){
+    delay(3200);
+  
+    servo1.write(102);
+    delay(600);
+    servo1.write(80); //Reset
+    delay(2200);
+  
+    servo2.write(95);
+    delay(600);
+    servo2.write(73); //Reset
+    delay(2200);
+  
+    servo3.write(83);
+    delay(600);
+    servo3.write(95); //Reset
+    delay(2200);
+  
+    servo4.write(115);
+    delay(600);
+    servo4.write(128); //Reset
+    delay(500);    
+}
+
+
